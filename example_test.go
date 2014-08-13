da0f77d3bf54411c5a50dd14dd46535b427bde59
Bryan Matsuo <bryan.matsuo@gmail.com>
2014-08-13 06:21:24 -0700

    initial draft -- wrapping writers

---
+++ example_test.go
@@ -0,0 +1,43 @@
+package iowrap_test
+
+import (
+	"bufio"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/bmatsuo/sandbox/iowrap"
+)
+
+// This example shows how to perform buffered writes of gzipped data to a file.
+func Example() {
+	f, err := ioutil.TempFile("", "iowrap-example-")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer os.Remove(f.Name())
+
+	// compose a chain of writers using WrapFunc.
+	w := iowrap.NewWriter(f)
+	w.WrapFunc(func(w io.Writer) io.Writer { return bufio.NewWriter(w) })
+	w.WrapFunc(func(w io.Writer) io.Writer { return gzip.NewWriter(w) })
+
+	// data will first be gzipped and then buffered before being written to the
+	// file.
+	_, err = fmt.Fprintln(w, "hello iowrap")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// close/flush the gzip writer, write bufferred data to the file, and close
+	// the file.
+	err = w.Close()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// Output:
+}
